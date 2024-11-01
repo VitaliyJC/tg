8b4ae20882758d826d9b664bd2bc5223d540aa2f
Vitaliy <vitaliy.jc@yandex.ru>
2024-11-01 18:29:21 +0500

    add all func

--- components/admin.js
+++ components/admin.js
@@ -1,5 +1,5 @@
 import { exec } from "child_process";
-import { UserController, CodeController } from "../controllers";
+import { UserController, CodeController } from "../controllers/index.js";
 
 // Команда для проверки подключенных клиентов
 export const connectedUsers = (bot) => {
