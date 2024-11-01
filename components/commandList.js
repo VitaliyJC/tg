e1f186d4e1e46d5a71c1dd8bfa52a50491f04415
Vitaliy <vitaliy.jc@yandex.ru>
2024-11-01 20:29:10 +0500

    add all func

--- components/commandList.js
+++ components/commandList.js
@@ -11,19 +11,21 @@ export const commandList = (
   ];
 
   const adminCommands = [
+    { command: "add_code", description: "Добавить код доступа" },
+    { command: "list_codes", description: "Показать все коды доступа" },
+    { command: "delete_code", description: "Удалить код доступа" },
     {
       command: "connected_users",
       description: "Проверка активных пользователей",
     },
-    { command: "add_code", description: "Добавить код доступа" },
-    { command: "list_codes", description: "Показать все коды доступа" },
-    { command: "delete_code", description: "Удалить код доступа" },
-    { command: "delete_all_codes", description: "Удалить все коды доступа" },
-    { command: "delete_user", description: "Удалить пользователя" },
     {
       command: "list_users",
       description: "Показать зарегистрированных пользователей",
     },
+    { command: "id", description: "Показать Ваш ID" },
+    { command: "delete_user", description: "Удалить пользователя" },
+    { command: "delete_all_codes", description: "Удалить все коды доступа" },
+    { command: "registration", description: "Зарегистрироваться" },
   ];
 
   // Устанавливаем команды для всех пользователей
