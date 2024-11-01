09f8c8a70b415c52b73d0df66b6f5edbb4383e7c
Vitaliy <vitaliy.jc@yandex.ru>
2024-11-01 17:03:16 +0500

    add all func

--- models/Code.js
+++ models/Code.js
@@ -22,39 +22,39 @@ const CodeSchema = new mongoose.Schema(
   }
 );
 
-// CodeSchema.statics.validateCode = async function ({
-//   codeString,
-//   throwError = false,
-// }) {
-//   // Находим код по значению
-//   const code = await this.findOne({ code: codeString });
-//   console.log("Найденный код:", code);
+CodeSchema.statics.validateCode = async function ({
+  codeString,
+  throwError = false,
+}) {
+  // Находим код по значению
+  const code = await this.findOne({ code: codeString });
+  console.log("Найденный код:", code);
 
-//   // Проверяем, существует ли код
-//   if (!code) {
-//     if (throwError) throw new Error("Код не найден.");
-//     return null; // Возвращаем null, если код не найден
-//   }
+  // Проверяем, существует ли код
+  if (!code) {
+    if (throwError) throw new Error("Код не найден.");
+    return null; // Возвращаем null, если код не найден
+  }
 
-//   // Проверяем, не истек ли срок действия кода и обновляем статус, если срок истек
-//   if (new Date() > code.expiryDate && code.status === "pending") {
-//     // Обновляем статус на "expired"
-//     await this.updateOne({ code: codeString }, { $set: { status: "expired" } });
-//     if (throwError) throw new Error("Срок действия кода истек.");
-//     console.log("Срок действия истек и код установлен в 'expired'");
-//     return null; // Возвращаем null, если срок истек
-//   }
+  // Проверяем, не истек ли срок действия кода и обновляем статус, если срок истек
+  if (new Date() > code.expiryDate && code.status === "pending") {
+    // Обновляем статус на "expired"
+    await this.updateOne({ code: codeString }, { $set: { status: "expired" } });
+    if (throwError) throw new Error("Срок действия кода истек.");
+    console.log("Срок действия истек и код установлен в 'expired'");
+    return null; // Возвращаем null, если срок истек
+  }
 
-//   // Проверяем, активен ли код (если статус не "pending" и не "activated")
-//   if (code.status !== "pending" && code.status !== "activated") {
-//     if (throwError) throw new Error("Код уже активирован или истек.");
-//     console.log("Код неактивен.");
-//     return null; // Возвращаем null, если код неактивен
-//   }
+  // Проверяем, активен ли код (если статус не "pending" и не "activated")
+  if (code.status !== "pending" && code.status !== "activated") {
+    if (throwError) throw new Error("Код уже активирован или истек.");
+    console.log("Код неактивен.");
+    return null; // Возвращаем null, если код неактивен
+  }
 
-//   // Если все проверки пройдены, возвращаем объект кода
-//   console.log("Код прошел все проверки:", code);
-//   return code;
-// };
+  // Если все проверки пройдены, возвращаем объект кода
+  console.log("Код прошел все проверки:", code);
+  return code;
+};
 
 export default mongoose.model("Code", CodeSchema);
