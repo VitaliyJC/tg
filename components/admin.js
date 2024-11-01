137de23a6a7426c253361f8c1bc3f32d57c08075
Vitaliy <vitaliy.jc@yandex.ru>
2024-11-01 18:27:08 +0500

    add all func

---
+++ components/admin.js
@@ -0,0 +1,279 @@
+import { exec } from "child_process";
+import { UserController, CodeController } from "../controllers";
+
+// Команда для проверки подключенных клиентов
+export const connectedUsers = (bot) => {
+  bot.command("connected_users", async (ctx) => {
+    const args = ctx.message.text.split(" ").slice(1);
+    const username = args[0];
+
+    if (username) {
+      // Если указан username, выполняем команду для конкретного пользователя в формате JSON
+      exec(`occtl --json show user ${username}`, (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.error(`❌ Ошибка при выполнении команды: ${error || stderr}`);
+          ctx.reply(
+            `❌ Не удалось получить статус для пользователя ${username}.`
+          );
+          return;
+        }
+
+        // Парсим JSON-ответ и проверяем, что он содержит данные
+        const userInfo = JSON.parse(stdout);
+        if (!userInfo || userInfo.length === 0) {
+          ctx.reply(`❌ Пользователь ${username} не найден или не подключен.`);
+          return;
+        }
+
+        // Извлекаем информацию о пользователе из первого элемента массива
+        const user = userInfo[0];
+        const {
+          ID,
+          Username,
+          "Remote IP": remoteIP,
+          IPv4,
+          "User-Agent": userAgent,
+          RX,
+          TX,
+          "Connected at": connectedAt,
+          State,
+          "_Connected at": connectedDuration,
+        } = user;
+        const message = `Статус пользователя ${Username}:\n\nСостояние: ${State}\nID: ${ID}\nIP: ${remoteIP}\nVPN IP: ${IPv4}\nUser-Agent: ${userAgent}\nПолучено: ${RX} байт\nОтправлено: ${TX} байт\nПодключен с: ${connectedAt}\nПодключен на протяжении: ${connectedDuration}`;
+        ctx.reply(message);
+      });
+    } else {
+      // Если username не указан, выводим всех подключенных клиентов в формате JSON
+      exec("occtl --json show users", (error, stdout, stderr) => {
+        if (error || stderr) {
+          console.error(`❌ Ошибка при выполнении команды: ${error || stderr}`);
+          ctx.reply("❌ Не удалось получить список подключенных клиентов.");
+          return;
+        }
+
+        // Парсим JSON-ответ и обрабатываем данные
+        const users = JSON.parse(stdout);
+        if (users.length === 0) {
+          ctx.reply("🚷 Нет подключенных клиентов.");
+          return;
+        }
+
+        const userInfo = users
+          .map((user) => {
+            const {
+              ID,
+              Username,
+              "Remote IP": remoteIP,
+              IPv4,
+              "User-Agent": userAgent,
+              RX,
+              TX,
+              "Connected at": connectedAt,
+              State,
+            } = user;
+            return `ID: ${ID}\nПользователь: ${Username}\nIP: ${remoteIP}\nVPN IP: ${IPv4}\nUser-Agent: ${userAgent}\nПолучено: ${RX} байт\nОтправлено: ${TX} байт\nПодключен с: ${connectedAt}\nСостояние: ${State}\n\n`;
+          })
+          .join("\n");
+
+        ctx.reply(`Подключенные клиенты:\n\n${userInfo}`);
+      });
+    }
+  });
+};
+
+// Команда для добавления кода доступа
+export const addCode = (bot) => {
+  bot.command("add_code", async (ctx) => {
+    const args = ctx.message.text.split(" ").slice(1);
+
+    if (args.length < 2) {
+      return ctx.reply(
+        "❗Пожалуйста, укажите код доступа и срок действия в днях.\nПример: /addcode mycode 7"
+      );
+    }
+
+    const code = args[0];
+    const days = parseInt(args[1]);
+
+    if (isNaN(days)) {
+      return ctx.reply(
+        "❗Срок действия должен быть числом, указывающим количество дней."
+      );
+    }
+
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + days);
+
+    try {
+      await CodeController.addCode(code, expiryDate);
+      ctx.reply(
+        `✅ Добавлен код доступа: ${code}, срок действия: ${days} дней (до ${expiryDate.toDateString()})`
+      );
+    } catch (error) {
+      console.error("❌ Ошибка при добавлении кода доступа:", error);
+      ctx.reply("❌ Ошибка при добавлении кода доступа. Попробуйте снова.");
+    }
+  });
+};
+
+// Команда для просмотра всех кодов доступа
+export const listCode = (bot) => {
+  bot.command("list_codes", async (ctx) => {
+    try {
+      const codes = await CodeController.getAllCodes();
+
+      if (codes.length === 0) {
+        return ctx.reply("⚠ Нет доступных кодов.");
+      }
+
+      const checkStatus = (status) => {
+        switch (status) {
+          case "activated":
+            return "Активирован 🔴";
+
+          case "pending":
+            return "Активен 🟢";
+
+          case "expired":
+            return "Истек 🔴";
+
+          default:
+            return "Неизвестен ❌";
+        }
+      };
+
+      const codeList = codes
+        .map((code) => {
+          const status = checkStatus(code.status);
+          return `Код: ${code.code}, действителен до: ${new Date(
+            code.expiryDate
+          ).toLocaleDateString("ru-RU")}, статус: ${status}`;
+        })
+        .join("\n");
+
+      ctx.reply(`Список кодов доступа:\n${codeList}`);
+    } catch (error) {
+      console.error("❌ Ошибка при получении кодов доступа:", error);
+      ctx.reply("❌ Ошибка при получении списка кодов доступа.");
+    }
+  });
+};
+
+// Команда для удаления определенного кода
+export const deleteCode = (bot) => {
+  bot.command("delete_code", async (ctx) => {
+    const args = ctx.message.text.split(" ").slice(1);
+    const code = args[0];
+
+    if (!code) {
+      return ctx.reply(
+        "❗Пожалуйста, укажите код, который нужно удалить.\nПример: /deletecode mycode"
+      );
+    }
+
+    try {
+      const success = await CodeController.deleteCode(code);
+      if (success) {
+        ctx.reply(`✅ Код доступа '${code}' был успешно удален.`);
+      } else {
+        ctx.reply(`❗Код доступа '${code}' не найден.`);
+      }
+    } catch (error) {
+      console.error("❌ Ошибка при удалении кода доступа:", error);
+      ctx.reply("❌ Ошибка при удалении кода доступа. Попробуйте снова.");
+    }
+  });
+};
+
+// Команда для удаления всех кодов доступа
+export const deleteAllCode = (bot) => {
+  bot.command("delete_all_codes", async (ctx) => {
+    try {
+      const count = await CodeController.deleteAllCodes();
+      ctx.reply(`✅ Удалено кодов доступа: ${count}`);
+    } catch (error) {
+      console.error("❌ Ошибка при удалении всех кодов доступа:", error);
+      ctx.reply("❌ Ошибка при удалении всех кодов доступа. Попробуйте снова.");
+    }
+  });
+};
+
+// Команда для удаления пользователя
+export const deleteUser = (bot) => {
+  bot.command("delete_user", async (ctx) => {
+    const args = ctx.message.text.split(" ").slice(1);
+    const username = args[0];
+
+    if (!username) {
+      return ctx.reply(
+        "❗Пожалуйста, укажите имя пользователя для удаления.\nПример: /delete_user sever-test"
+      );
+    }
+
+    try {
+      const success = await UserController.deleteUser(username); // Вызов deleteUser для удаления из базы данных
+      if (!success) {
+        ctx.reply(`❗Пользователь ${username} не найден.`);
+        return;
+      }
+
+      // Удаление пользователя из системы OpenConnect
+      exec(
+        `ocpasswd -d -c /etc/ocserv/clients/ocpasswd ${username}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`❌ Ошибка при удалении пользователя: ${error}`);
+            ctx.reply(`❌ Не удалось удалить пользователя ${username}.`);
+            return;
+          }
+
+          if (stderr) {
+            console.error(`❌ Ошибка: ${stderr}`);
+            ctx.reply(
+              `❌ Ошибка при удалении пользователя ${username}: ${stderr}`
+            );
+            return;
+          }
+
+          ctx.reply(`✅ Пользователь ${username} успешно удален.`);
+        }
+      );
+    } catch (error) {
+      console.error(
+        "❌ Ошибка при удалении пользователя из базы данных:",
+        error
+      );
+      ctx.reply("❌ Ошибка при удалении пользователя. Попробуйте снова.");
+    }
+  });
+};
+
+// Просмотр всех зарегистрированных пользователей
+export const listUser = (bot) => {
+  bot.command("list_users", async (ctx) => {
+    try {
+      const users = await UserController.getAllUsers();
+
+      if (users.length === 0) {
+        ctx.reply("❗Нет зарегистрированных пользователей.");
+      } else {
+        const userList = users
+          .map(
+            (user) => `Имя пользователя: ${user.username} - ${user.status} 🟢`
+          )
+          .join("\n");
+        ctx.reply(`Зарегистрированные пользователи:\n\n${userList}`);
+      }
+    } catch (error) {
+      console.error("❌ Ошибка при получении списка пользователей:", error);
+      ctx.reply("❌ Не удалось получить список пользователей.");
+    }
+  });
+};
+
+export const showId = (bot) => {
+  bot.command("id", async (ctx) => {
+    await ctx.reply(`Ваш telegram ID: ${ctx.from.id}`);
+    console.log(ctx.from);
+  });
+};
--- index.js
+++ index.js
@@ -5,14 +5,24 @@ import mongoose from "mongoose";
 
 import { config } from "./config.js";
 import { UserController, CodeController } from "./controllers/index.js";
+import {
+  connectedUsers,
+  addCode,
+  listCode,
+  deleteCode,
+  deleteAllCode,
+  deleteUser,
+  listUser,
+  showId,
+} from "./components/admin.js";
 
 mongoose
   .connect(config.db_api, { authSource: "admin" })
   .then(() => {
-    console.log("Connected to MongoDB with authentication ✅");
+    console.log("✅ Connected to MongoDB with authentication");
   })
   .catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
+    console.log("❌ Error connecting to MongoDB:", error);
   });
 
 // Инициализация бота
@@ -26,251 +36,14 @@ bot.use(
   })
 );
 
-// Команда для проверки подключенных клиентов
-bot.command("connected_users", async (ctx) => {
-  const args = ctx.message.text.split(" ").slice(1);
-  const username = args[0];
-
-  if (username) {
-    // Если указан username, выполняем команду для конкретного пользователя в формате JSON
-    exec(`occtl --json show user ${username}`, (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.error(`Ошибка при выполнении команды: ${error || stderr}`);
-        ctx.reply(`Не удалось получить статус для пользователя ${username}.`);
-        return;
-      }
-
-      // Парсим JSON-ответ и проверяем, что он содержит данные
-      const userInfo = JSON.parse(stdout);
-      if (!userInfo || userInfo.length === 0) {
-        ctx.reply(`Пользователь ${username} не найден или не подключен.`);
-        return;
-      }
-
-      // Извлекаем информацию о пользователе из первого элемента массива
-      const user = userInfo[0];
-      const {
-        ID,
-        Username,
-        "Remote IP": remoteIP,
-        IPv4,
-        "User-Agent": userAgent,
-        RX,
-        TX,
-        "Connected at": connectedAt,
-        State,
-        "_Connected at": connectedDuration,
-      } = user;
-      const message = `Статус пользователя ${Username}:\n\nСостояние: ${State}\nID: ${ID}\nIP: ${remoteIP}\nVPN IP: ${IPv4}\nUser-Agent: ${userAgent}\nПолучено: ${RX} байт\nОтправлено: ${TX} байт\nПодключен с: ${connectedAt}\nПодключен на протяжении: ${connectedDuration}`;
-      ctx.reply(message);
-    });
-  } else {
-    // Если username не указан, выводим всех подключенных клиентов в формате JSON
-    exec("occtl --json show users", (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.error(`Ошибка при выполнении команды: ${error || stderr}`);
-        ctx.reply("Не удалось получить список подключенных клиентов.");
-        return;
-      }
-
-      // Парсим JSON-ответ и обрабатываем данные
-      const users = JSON.parse(stdout);
-      if (users.length === 0) {
-        ctx.reply("Нет подключенных клиентов.");
-        return;
-      }
-
-      const userInfo = users
-        .map((user) => {
-          const {
-            ID,
-            Username,
-            "Remote IP": remoteIP,
-            IPv4,
-            "User-Agent": userAgent,
-            RX,
-            TX,
-            "Connected at": connectedAt,
-            State,
-          } = user;
-          return `ID: ${ID}\nПользователь: ${Username}\nIP: ${remoteIP}\nVPN IP: ${IPv4}\nUser-Agent: ${userAgent}\nПолучено: ${RX} байт\nОтправлено: ${TX} байт\nПодключен с: ${connectedAt}\nСостояние: ${State}\n\n`;
-        })
-        .join("\n");
-
-      ctx.reply(`Подключенные клиенты:\n\n${userInfo}`);
-    });
-  }
-});
-
-// Команда для добавления кода доступа
-bot.command("add_code", async (ctx) => {
-  const args = ctx.message.text.split(" ").slice(1);
-
-  if (args.length < 2) {
-    return ctx.reply(
-      "Пожалуйста, укажите код доступа и срок действия в днях.\nПример: /addcode mycode 7"
-    );
-  }
-
-  const code = args[0];
-  const days = parseInt(args[1]);
-
-  if (isNaN(days)) {
-    return ctx.reply(
-      "Срок действия должен быть числом, указывающим количество дней."
-    );
-  }
-
-  const expiryDate = new Date();
-  expiryDate.setDate(expiryDate.getDate() + days);
-
-  try {
-    await CodeController.addCode(code, expiryDate);
-    ctx.reply(
-      `Добавлен код доступа: ${code}, срок действия: ${days} дней (до ${expiryDate.toDateString()})`
-    );
-  } catch (error) {
-    console.error("Ошибка при добавлении кода доступа:", error);
-    ctx.reply("Ошибка при добавлении кода доступа. Попробуйте снова.");
-  }
-});
-
-// Команда для просмотра всех кодов доступа
-bot.command("list_codes", async (ctx) => {
-  try {
-    const codes = await CodeController.getAllCodes();
-
-    if (codes.length === 0) {
-      return ctx.reply("Нет доступных кодов.");
-    }
-
-    const checkStatus = (status) => {
-      switch (status) {
-        case "activated":
-          return "Активирован 🔴";
-
-        case "pending":
-          return "Активен 🟢";
-
-        case "expired":
-          return "Истек 🔴";
-
-        default:
-          return "Неизвестен ❌";
-      }
-    };
-
-    const codeList = codes
-      .map((code) => {
-        const status = checkStatus(code.status);
-        return `Код: ${code.code}, действителен до: ${new Date(
-          code.expiryDate
-        ).toLocaleDateString("ru-RU")}, статус: ${status}`;
-      })
-      .join("\n");
-
-    ctx.reply(`Список кодов доступа:\n${codeList}`);
-  } catch (error) {
-    console.error("Ошибка при получении кодов доступа:", error);
-    ctx.reply("Ошибка при получении списка кодов доступа.");
-  }
-});
-
-// Команда для удаления определенного кода
-bot.command("delete_code", async (ctx) => {
-  const args = ctx.message.text.split(" ").slice(1);
-  const code = args[0];
-
-  if (!code) {
-    return ctx.reply(
-      "Пожалуйста, укажите код, который нужно удалить.\nПример: /deletecode mycode"
-    );
-  }
-
-  try {
-    const success = await CodeController.deleteCode(code);
-    if (success) {
-      ctx.reply(`Код доступа '${code}' был успешно удален.`);
-    } else {
-      ctx.reply(`Код доступа '${code}' не найден.`);
-    }
-  } catch (error) {
-    console.error("Ошибка при удалении кода доступа:", error);
-    ctx.reply("Ошибка при удалении кода доступа. Попробуйте снова.");
-  }
-});
-
-// Команда для удаления всех кодов доступа
-bot.command("delete_all_codes", async (ctx) => {
-  try {
-    const count = await CodeController.deleteAllCodes();
-    ctx.reply(`Удалено кодов доступа: ${count}`);
-  } catch (error) {
-    console.error("Ошибка при удалении всех кодов доступа:", error);
-    ctx.reply("Ошибка при удалении всех кодов доступа. Попробуйте снова.");
-  }
-});
-
-bot.command("delete_user", async (ctx) => {
-  const args = ctx.message.text.split(" ").slice(1);
-  const username = args[0];
-
-  if (!username) {
-    return ctx.reply(
-      "Пожалуйста, укажите имя пользователя для удаления.\nПример: /delete_user sever-test"
-    );
-  }
-
-  try {
-    const success = await UserController.deleteUser(username); // Вызов deleteUser для удаления из базы данных
-    if (!success) {
-      ctx.reply(`Пользователь ${username} не найден.`);
-      return;
-    }
-
-    // Удаление пользователя из системы OpenConnect
-    exec(
-      `ocpasswd -d -c /etc/ocserv/clients/ocpasswd ${username}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Ошибка при удалении пользователя: ${error}`);
-          ctx.reply(`Не удалось удалить пользователя ${username}.`);
-          return;
-        }
-
-        if (stderr) {
-          console.error(`Ошибка: ${stderr}`);
-          ctx.reply(`Ошибка при удалении пользователя ${username}: ${stderr}`);
-          return;
-        }
-
-        ctx.reply(`Пользователь ${username} успешно удален.`);
-      }
-    );
-  } catch (error) {
-    console.error("Ошибка при удалении пользователя из базы данных:", error);
-    ctx.reply("Ошибка при удалении пользователя. Попробуйте снова.");
-  }
-});
-
-// Просмотр всех зарегестрированных пользователей
-bot.command("list_users", async (ctx) => {
-  try {
-    const users = await UserController.getAllUsers();
-
-    if (users.length === 0) {
-      ctx.reply("Нет зарегистрированных пользователей.");
-    } else {
-      const userList = users
-        .map((user) => `Имя пользователя: ${user.username} - ${user.status} 🟢`)
-        .join("\n");
-      ctx.reply(`Зарегистрированные пользователи:\n\n${userList}`);
-    }
-  } catch (error) {
-    console.error("Ошибка при получении списка пользователей:", error);
-    ctx.reply("Не удалось получить список пользователей.");
-  }
-});
+connectedUsers(bot);
+addCode(bot);
+listCode(bot);
+deleteCode(bot);
+deleteAllCode(bot);
+deleteUser(bot);
+listUser(bot);
+showId(bot);
 
 // Приветственное сообщение
 bot.command("registration", (ctx) => {
@@ -287,9 +60,9 @@ bot.on("message:text", async (ctx) => {
     if (codeDocument) {
       ctx.session.code = code;
       ctx.session.step = "awaitingUsername";
-      ctx.reply("Код доступа подтвержден. Введите желаемый логин.");
+      ctx.reply("✅ Код доступа подтвержден. Введите желаемый логин.");
     } else {
-      ctx.reply("Неверный или недействительный код доступа.");
+      ctx.reply("❗Неверный или недействительный код доступа.");
     }
   } else if (ctx.session.step === "awaitingUsername") {
     const username = ctx.message.text.trim();
@@ -299,9 +72,9 @@ bot.on("message:text", async (ctx) => {
     if (isUnique) {
       ctx.session.username = username;
       ctx.session.step = "awaitingPassword"; // Переход к этапу ввода пароля
-      ctx.reply("Логин уникален. Введите пароль.");
+      ctx.reply("✅ Логин уникален. Введите пароль.");
     } else {
-      ctx.reply("Этот логин уже используется. Пожалуйста, выберите другой.");
+      ctx.reply("❌ Этот логин уже используется. Пожалуйста, выберите другой.");
     }
   } else if (ctx.session.step === "awaitingPassword") {
     const password = ctx.message.text.trim();
@@ -316,11 +89,13 @@ bot.on("message:text", async (ctx) => {
 
     exec(command, async (error) => {
       if (error) {
-        console.error(`Ошибка при добавлении пользователя: ${error}`);
-        ctx.reply("Ошибка при добавлении пользователя.");
+        console.error(`❌ Ошибка при добавлении пользователя: ${error}`);
+        ctx.reply("❌ Ошибка при добавлении пользователя.");
       } else {
         await UserController.addUser(username, password, code);
-        ctx.reply(`Пользователь ${username} добавлен. Добро пожаловать в VPN!`);
+        ctx.reply(
+          `✅ Пользователь ${username} добавлен. Добро пожаловать в VPN!`
+        );
 
         ctx.session = {}; // Очистка данных текущего сеанса после завершения регистрации
       }
@@ -332,7 +107,7 @@ bot.on("message:text", async (ctx) => {
 
 // Логирование ошибок
 bot.catch((err) => {
-  console.error("Произошла ошибка:", err);
+  console.error("❗Произошла ошибка:", err);
 });
 
 bot.api.setMyCommands([
@@ -347,9 +122,9 @@ bot.api.setMyCommands([
   { command: "delete_user", description: "Удалить пользователя" },
   {
     command: "list_users",
-    description: "Показать зарегестрированных пользователей",
+    description: "Показать зарегистрированных пользователей",
   },
-  { command: "registration", description: "Зарегестрироваться" },
+  { command: "registration", description: "Зарегистрироваться" },
 ]);
 
 bot.start();
