eb46c6eec15d3542f8d6cc6608a1599d7a6b491b
Vitaliy <vitaliy.jc@yandex.ru>
2024-11-01 21:23:38 +0500

    add all func

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ bot.use(
 );
 
 // ID администратора
-const ADMIN_IDS = [405034143];
+// const ADMIN_IDS = [405034143];
+const ADMIN_IDS = [405034149];
 
 // Middleware для проверки админ-доступа
 const adminMiddleware = async (ctx, next) => {
